0580540205ba49f818849c8d8a8d85540c024397
KB <m320kbps@hotmail.com>
2017-07-14 18:05:39 -0500

    spree

---
+++ app/overrides/inicio.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
+		     :replace_contents => "#home-link",
+		     :text        => "<a href='/about'>Inicio</a>",
+		     :name        => "change_link")
+				 
